7ee915558e3bb0c0dec8e4410e63a5faf60a4404
juaszdelove1-prog <juaszdelove1@gmail.com>
2026-09-20 11:23:02 +0300

    Fix Cloudflare Worker TypeScript build

--- src/worker.ts
+++ src/worker.ts
@@ -1,2 +1,11 @@
-interface Env { ASSETS: Fetcher }
-export default { async fetch(request: Request, env: Env): Promise<Response> { return env.ASSETS.fetch(request) } } satisfies ExportedHandler<Env>
+/// <reference types="@cloudflare/workers-types" />
+
+interface Env {
+  ASSETS: Fetcher
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    return env.ASSETS.fetch(request)
+  },
+} satisfies ExportedHandler<Env>
