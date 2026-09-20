4957e670724079e605fd0176f7ce89dd8ed60963
juaszdelove1-prog <juaszdelove1@gmail.com>
2026-09-20 16:19:47 +0300

    Serve WAF public SEO routes through Cloudflare

--- src/worker.ts
+++ src/worker.ts
@@ -4,8 +4,24 @@ interface Env {
   ASSETS: Fetcher
 }
 
+const publicRoutes = new Set([
+  '/about',
+  '/conservation',
+  '/sustainable-tourism',
+  '/tanzania',
+  '/news',
+  '/contact',
+])
+
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url)
+
+    if (publicRoutes.has(url.pathname)) {
+      const home = new URL('/', url)
+      return env.ASSETS.fetch(new Request(home, request))
+    }
+
     return env.ASSETS.fetch(request)
   },
 } satisfies ExportedHandler<Env>
